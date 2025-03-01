5ac520c17d654e26ab41ecebcdcb010b19699b09
Ambre Saint-Marc <saintmarc.ambre@gmail.com>
2025-03-01 23:24:52 +0100

    Creation of components and established routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ArtisanListComponent } from './components/artisan-list/artisan-list.component';
+import { ArtisanPageComponent } from './components/artisan-page/artisan-page.component';
+import { PageInBuildingComponent } from './components/page-in-building/page-in-building.component';
+import { Page404Component } from './components/page404/page404.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path:'',
+        component: HomeComponent,
+        
+    },
+    {
+        path:'list',
+        component: ArtisanListComponent,
+        
+    },
+    {
+        path:'details',
+        component: ArtisanPageComponent,
+        
+    },
+    {
+        path:'in-progress',
+        component: PageInBuildingComponent,
+        
+    },
+    {
+        path:'page-404',
+        component: Page404Component,
+        
+    }
+];
