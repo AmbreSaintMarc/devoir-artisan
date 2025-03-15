e46ad68c2dd64b136b48f017c21060434c96e3b0
Ambre Saint-Marc <saintmarc.ambre@gmail.com>
2025-03-15 16:37:23 +0100

    Styled the artisan's card

--- src/app/components/artisan/artisan.component.ts
+++ src/app/components/artisan/artisan.component.ts
@@ -1,26 +1,22 @@
-import { Component, inject, input, OnInit } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Artisan } from '../../artisan';
 import { RouterLink } from '@angular/router';
 import { Router } from '@angular/router';
 import { ArtisansService } from '../../artisans.service';
-import { NgStyle } from '@angular/common';
+
 
 @Component({
   selector: 'app-artisan',
-  imports: [CommonModule, RouterLink, NgStyle],
+  imports: [CommonModule, RouterLink],
   templateUrl: './artisan.component.html',
   styleUrl: './artisan.component.scss'
 })
-export class ArtisanComponent implements OnInit{
+export class ArtisanComponent {
   artisan = input.required<Artisan>()
   artisansService = inject(ArtisansService);
   artisansList: Artisan[] = []
-  noteBackground: string = 'linear-gradient(to right, gold %, transparent 33%)'
-  paramNoteBackground : string = '% ,transparent 33.333%,transparent 100%)'
-  note: number = 1;
-  result: any
-  
+
   
 
   //how to display details page on click 
@@ -30,17 +26,8 @@ export class ArtisanComponent implements OnInit{
     this.router.navigateByUrl("details/:id")
   }
 
-  getNote(note: number) {
-    this.artisansList = this.artisansList.filter((artisan) =>
-    artisan.note === note)
-    
-  }
 
-  ngOnInit(): void {
-    this.result =
-      this.noteBackground = 'linear-gradient(to right,gold'  + (this.note.valueOf()*10)*2  + this.paramNoteBackground ; console.log(this.noteBackground)
-    
-  }
+
 
 }
 
